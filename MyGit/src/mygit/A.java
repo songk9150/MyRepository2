77399cdbfb2a0905ddc8a8a0f147578d90a150d2
song-useung <song-useung@218.38.137.28>
2023-12-21 13:38:03 +0900

    A,B 수

--- MyGit/src/mygit/A.java
+++ MyGit/src/mygit/A.java
@@ -3,5 +3,7 @@ package mygit;
 public class A {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
+		B b = new B();
+		b.sayHello("A");
 	}
 }
--- MyGit/src/mygit/B.java
+++ MyGit/src/mygit/B.java
@@ -3,5 +3,6 @@ package mygit;
 public class B {
 	public void sayHello(String toWhom) {
 		System.out.println("Hello!"+ toWhom);
+		System.out.println("Your changed in  master branch");
 	}
 }
