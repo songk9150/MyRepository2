c99ef7794e962af093eec893a554c03ad1236838
song-useung <song-useung@218.38.137.28>
2023-12-21 13:40:39 +0900

    A,C 수정.

--- MyGit/src/mygit/A.java
+++ MyGit/src/mygit/A.java
@@ -3,5 +3,7 @@ package mygit;
 public class A {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
+		C c = new C();
+		c.sayBye("C");
 	}
 }
--- MyGit/src/mygit/C.java
+++ MyGit/src/mygit/C.java
@@ -3,5 +3,6 @@ package mygit;
 public class C {
 	public void sayBye(String toWhom) {
 		System.out.println("Bye!"+ toWhom);
+		System.out.println("C 수정완료");
 	}
 }
